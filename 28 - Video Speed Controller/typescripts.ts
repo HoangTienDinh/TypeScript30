a14a326eb4aef996af254a1563d4c6d0ceccc084
William Wager <wwags33@gmail.com>
2020-03-04 13:44:45 -0500

    Adding remaining TS files

---
+++ 28 - Video Speed Controller/typescripts.ts
@@ -0,0 +1,10 @@
+/**
+ * JavaScript30 by Wes Bos, https://javascript30.com/
+ * TypeScript implementation by Will Wager
+ * Project:
+ * Concepts:
+ * Key takeaways:
+ * Sidenotes:
+ * Compilation command:
+ *   tsc --removeComments --strictNullChecks --noImplicitAny --target es5 typescripts.ts
+ */
